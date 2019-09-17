e0fee2e794aa73b456d6619f1c93ada52067de96
tejojr <ammar.fiky@gmail.com>
2019-09-17 22:42:21 +0700

    cinth

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require("dotenv").config()
-const express = require('express');
-const app = express();
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const serverPOST = 3306;
-const PORT = process.env.PORT || serverPOST;
+const express = require("express")
+const app = express()
+const logger = require("morgan")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const serverPOST = 3306
+const PORT = process.env.PORT || serverPOST
 //add cloudinary config
 const { cloudinaryConfig } = require("./src/config/cloudinaryConfig")
 
-const AuthRoute = require('./src/routes/auth');
-const UserRoute = require('./src/routes/user');
-const PartnerRoute = require('./src/routes/partner');
+const AuthRoute = require("./src/routes/auth")
+const UserRoute = require("./src/routes/user")
+const PartnerRoute = require("./src/routes/partner")
+const RoomRoute = require("./src/routes/rooms")
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
@@ -27,8 +28,9 @@ app.use(
 )
 
 // Route for Auth
-app.use('/', AuthRoute);
+app.use("/", AuthRoute)
 // Rouute for User
-app.use('/user', UserRoute);
+app.use("/user", UserRoute)
 // Route for Partner
-app.use('/partner', PartnerRoute);
+app.use("/partner", PartnerRoute)
+app.use("/rooms", RoomRoute)
